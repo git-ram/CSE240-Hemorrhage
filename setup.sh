5b5ece8e0735a7f211e195067b7976203b0c1544
Abhay Pande <abhaypande@Abhays-MacBook-Pro.local>
2020-03-13 03:02:08 -0700

    updated setup

--- setup.sh
+++ setup.sh
@@ -19,8 +19,8 @@ nvidia-smi
 pip3 install numpy 
 pip3 install pandas
 #pip3 uninstall tensorflow
-pip3 install --upgrade tensorflow-gpu
-pip3 install keras
+#pip3 install --upgrade tensorflow-gpu
+#pip3 install keras
 pip3 install sklearn
 pip3 install matplotlib
 pip3 install cachetools
