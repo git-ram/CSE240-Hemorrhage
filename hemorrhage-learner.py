07277e1d594618867a9494ad174be6532b5e1ab0
git-ram <25213147+git-ram@users.noreply.github.com>
2020-03-13 00:48:06 -0700

    minor change on gpu access

--- hemorrhage-learner.py
+++ hemorrhage-learner.py
@@ -33,25 +33,6 @@ import tensorflow as tf
 from keras.layers.advanced_activations import LeakyReLU
 import keras.backend.tensorflow_backend as tfback
 
-# Code to access four GPUs: https://github.com/keras-team/keras/issues/13684
-"""
-print("tf.__version__ is", tf.__version__)
-print("tf.keras.__version__ is:", tf.keras.__version__)
-
-def _get_available_gpus():
-    """Get a list of available gpu devices (formatted as strings).
-
-    # Returns
-        A list of available GPU devices.
-    """
-    #global _LOCAL_DEVICES
-    if tfback._LOCAL_DEVICES is None:
-        devices = tf.config.list_logical_devices()
-        tfback._LOCAL_DEVICES = [x.name for x in devices]
-    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
-
-tfback._get_available_gpus = _get_available_gpus
-"""
 
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
