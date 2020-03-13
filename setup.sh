67b823d65405f4ed4ff70a8c52ba1ffe4f5d63a7
Abhay Pande <abhaypande@Abhays-MacBook-Pro.local>
2020-03-13 01:20:46 -0700

    changes package on setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip3 install numpy
 pip3 install pandas
 pip3 install keras
 pip3 install pydicom
-pip3 install tensorflow
+pip3 install --upgrade tensorflow-gpu
 pip3 install sklearn
 pip3 install matplotlib
 pip3 install cachetools
