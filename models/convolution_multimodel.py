128684bbd531a950925d272b75d87a0e23faf412
Abhay Pande <abhaypande@Abhays-MacBook-Pro.local>
2020-03-14 05:37:50 -0700

    added

--- models/convolution_multimodel.py
+++ models/convolution_multimodel.py
@@ -27,10 +27,9 @@ class CNNMultilabel(Model):
             self.input_shape = (img_rows,img_cols,1)
 
         self.model = Sequential()
-        self.model.add(Conv2D(32, kernel_size=(3, 3),
+        self.model.add(Conv2D(16, kernel_size=(3, 3),
                          activation='relu',
                          input_shape=self.input_shape))
-        self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
         self.model.add(Flatten())
